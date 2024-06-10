e98cd4b0f879482457dec8667bbe61d7f35d0a10
patichandana <iampati.official@gmail.com>
2024-06-10 10:05:26 +0530

    modified code to ping multiple hosts and send the host status to the reply

--- isInternetAlive.js
+++ isInternetAlive.js
@@ -1,13 +1,20 @@
 const ping = require("ping")
 
 const isInternetAlive = (request, reply) => {
-    const hosts = ["nuc.local"]
-    hosts.forEach((host) => {
-        ping.sys.probe(host, function(isAlive) {
-            console.log(`is_internet_alive(domain="${host}") ` +  Number(isAlive));
-            reply.send(`is_internet_alive(domain="${host}) ` +  Number(isAlive))        
-        })
+    const hosts = ["nuc.local", "google.com"]
+    let isAlive = []
+    let response = "";
+    hosts.forEach(async (host, index) => {
+        await ping.sys.probe(host, function (websiteStatus) {
+            // console.log(`is_internet_alive(domain="${host}") ` +  Number(isAlive));
+            // console.log(host)
+            isAlive.push(websiteStatus);
+            response = response + `is_internet_alive{domain="${host}"} ` + Number(websiteStatus) + "\n";
+        });
+        if(isAlive.length == hosts.length)
+            reply.send(response);
     });
+    // reply.send(response)        
 }
 
 module.exports = {isInternetAlive: isInternetAlive}
