076583dc4a8fb9849d0abb1c4a6ab97ba0c321b3
Pati Chandana <pati.c.chandana@oracle.com>
2024-06-12 23:26:38 +0530

    moved the call back approach to promises approach

--- isInternetAlive.js
+++ isInternetAlive.js
@@ -4,17 +4,21 @@ const isInternetAlive = (request, reply) => {
     const hosts = ["nuc.local", "google.com"]
     let isAlive = []
     let response = "";
-    hosts.forEach(async (host, index) => {
-        await ping.sys.probe(host, function (websiteStatus) {
-            // console.log(`is_internet_alive(domain="${host}") ` +  Number(isAlive));
-            // console.log(host)
+    let pingPromises = []
+    hosts.forEach((host, index) => {
+        pingPromises.push(new Promise((resolve) => {
+            ping.sys.probe(host, resolve);
+        }).then((websiteStatus) => {
             isAlive.push(websiteStatus);
             response = response + `is_internet_alive{domain="${host}"} ` + Number(websiteStatus) + "\n";
-        });
-        if(isAlive.length == hosts.length)
-            reply.send(response);
+        }));
     });
+    Promise.all(pingPromises).then(() => {
+        reply.send(response);
+    }).catch(() => {
+        reply.send(response)
+    })
     // reply.send(response)        
 }
 
-module.exports = {isInternetAlive: isInternetAlive}
+module.exports = { isInternetAlive: isInternetAlive }
